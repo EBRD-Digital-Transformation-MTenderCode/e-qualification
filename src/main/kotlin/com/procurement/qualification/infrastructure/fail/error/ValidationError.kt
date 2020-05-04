5b0e21db08fdfcda62760dd435a625cc3a60283f
katherine <kate.shaidurova@gmail.com>
2020-05-04 12:13:23 +0300

    ES-3129 Added InvalidPeriod and InvalidPeriodTerm validation errors

--- src/main/kotlin/com/procurement/qualification/infrastructure/fail/error/ValidationError.kt
+++ src/main/kotlin/com/procurement/qualification/infrastructure/fail/error/ValidationError.kt
@@ -6,6 +6,21 @@ sealed class ValidationError(
     numberError: String,
     override val description: String,
     val entityId: String? = null
-) : Fail.Error("VR-") {
+) : Fail.Error("VR") {
     override val code: String = prefix + numberError
+
+    sealed class CommandError(
+        numberError: String, description: String
+    ) : ValidationError(".COM-$numberError", description) {
+
+        class InvalidPeriod() : CommandError(
+            numberError = "7.1.3",
+            description = "Period start date must precede end date."
+        )
+
+        class InvalidPeriodTerm() : CommandError(
+            numberError = "7.1.4",
+            description = "Period duration is invalid."
+        )
+    }
 }
