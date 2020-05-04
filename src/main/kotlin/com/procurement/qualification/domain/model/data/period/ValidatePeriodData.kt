3d2122cf33c132108d88176d4f8f0b617775798e
katherine <kate.shaidurova@gmail.com>
2020-05-04 12:12:33 +0300

    ES-3129 Added ValidatePeriod data and converter

---
+++ src/main/kotlin/com/procurement/qualification/domain/model/data/period/ValidatePeriodData.kt
@@ -0,0 +1,35 @@
+package com.procurement.qualification.domain.model.data.period
+
+import com.procurement.qualification.application.model.parseDate
+import com.procurement.qualification.domain.functional.Result
+import com.procurement.qualification.domain.functional.asSuccess
+import com.procurement.qualification.infrastructure.fail.error.DataErrors
+import java.time.LocalDateTime
+
+class ValidatePeriodData private constructor(
+    val period: Period
+) {
+    companion object {
+        fun tryCreate(period: Period) = ValidatePeriodData(period).asSuccess<ValidatePeriodData, DataErrors>()
+    }
+
+    class Period private constructor(
+        val startDate: LocalDateTime,
+        val endDate: LocalDateTime
+    ) {
+        companion object {
+            private const val START_DATE_ATTRIBUTE_NAME = "startDate"
+            private const val END_DATE_ATTRIBUTE_NAME = "endDate"
+
+            fun tryCreate(startDate: String, endDate: String): Result<Period, DataErrors> {
+                val startDateParsed = parseDate(startDate, START_DATE_ATTRIBUTE_NAME)
+                    .orForwardFail { error -> return error }
+
+                val endDateParsed = parseDate(endDate, END_DATE_ATTRIBUTE_NAME)
+                    .orForwardFail { error -> return error }
+
+                return Period(startDate = startDateParsed, endDate = endDateParsed).asSuccess()
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/procurement/qualification/infrastructure/converter/ValidatePeriodConverter.kt
@@ -0,0 +1,24 @@
+package com.procurement.qualification.infrastructure.converter
+
+import com.procurement.qualification.domain.functional.Result
+import com.procurement.qualification.domain.functional.asSuccess
+import com.procurement.qualification.domain.model.data.period.ValidatePeriodContext
+import com.procurement.qualification.domain.model.data.period.ValidatePeriodData
+import com.procurement.qualification.infrastructure.fail.error.DataErrors
+import com.procurement.qualification.infrastructure.web.dto.command.CommandMessage
+import com.procurement.qualification.infrastructure.web.dto.command.country
+import com.procurement.qualification.infrastructure.web.dto.command.pmd
+import com.procurement.qualification.infrastructure.web.dto.request.period.ValidatePeriodRequest
+
+fun ValidatePeriodRequest.convert() = ValidatePeriodData.tryCreate(
+    period = period.convert()
+        .orForwardFail { error -> return error })
+
+fun ValidatePeriodRequest.Period.convert() =
+    ValidatePeriodData.Period.tryCreate(startDate = startDate, endDate = endDate)
+
+fun CommandMessage.toValidatePeriodContext(): Result<ValidatePeriodContext, DataErrors> =
+    ValidatePeriodContext(
+        country = country.orForwardFail { error -> return error },
+        pmd = pmd.orForwardFail { error -> return error }
+    ).asSuccess()
