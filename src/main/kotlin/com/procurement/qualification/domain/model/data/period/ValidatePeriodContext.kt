c228ad59bce256d5fe21c5d59cf46a52e82ec779
katherine <kate.shaidurova@gmail.com>
2020-05-04 12:12:14 +0300

    ES-3129 Added ValidatePeriod request and context

---
+++ src/main/kotlin/com/procurement/qualification/domain/model/data/period/ValidatePeriodContext.kt
@@ -0,0 +1,5 @@
+package com.procurement.qualification.domain.model.data.period
+
+import com.procurement.qualification.domain.enums.ProcurementMethod
+
+class ValidatePeriodContext(val country: String, val pmd: ProcurementMethod)
---
+++ src/main/kotlin/com/procurement/qualification/infrastructure/web/dto/request/period/ValidatePeriodRequest.kt
@@ -0,0 +1,12 @@
+package com.procurement.qualification.infrastructure.web.dto.request.period
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class ValidatePeriodRequest(
+    @param:JsonProperty("period") @field:JsonProperty("period") val period: Period
+) {
+    data class Period(
+        @param:JsonProperty("startDate") @field:JsonProperty("startDate") val startDate: String,
+        @param:JsonProperty("endDate") @field:JsonProperty("endDate") val endDate: String
+    )
+}
