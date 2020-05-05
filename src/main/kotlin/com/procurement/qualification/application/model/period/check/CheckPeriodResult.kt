e5d29cf2b74a0497d21ca8712568b95f5e80f20f
katherine <kate.shaidurova@gmail.com>
2020-05-05 20:46:22 +0300

    ES-3161 Changed String to LocalDateTime, fixed boolean with 'is' in naming in CheckPeriodResult.kt

--- src/main/kotlin/com/procurement/qualification/application/model/period/check/CheckPeriodResult.kt
+++ src/main/kotlin/com/procurement/qualification/application/model/period/check/CheckPeriodResult.kt
@@ -1,17 +1,18 @@
 package com.procurement.qualification.application.model.period.check
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDateTime
 
 data class CheckPeriodResult(
-    @param:JsonProperty("isPreQualificationPeriodChanged") @field:JsonProperty("isPreQualificationPeriodChanged") val isPreQualificationPeriodChanged: Boolean,
+    @param:JsonProperty("isPreQualificationPeriodChanged") @field:JsonProperty("isPreQualificationPeriodChanged") val preQualificationPeriodChanged: Boolean,
     @param:JsonProperty("preQualification") @field:JsonProperty("preQualification") val preQualification: PreQualification
 ) {
     data class PreQualification(
         @param:JsonProperty("period") @field:JsonProperty("period") val period: Period
     ) {
         data class Period(
-            @param:JsonProperty("startDate") @field:JsonProperty("startDate") val startDate: String,
-            @param:JsonProperty("endDate") @field:JsonProperty("endDate") val endDate: String
+            @param:JsonProperty("startDate") @field:JsonProperty("startDate") val startDate: LocalDateTime,
+            @param:JsonProperty("endDate") @field:JsonProperty("endDate") val endDate: LocalDateTime
         )
     }
 }
