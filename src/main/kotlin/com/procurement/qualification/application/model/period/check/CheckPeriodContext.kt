c5b77c1a19208e5e2575952c1dd23f28cfb4180b
katherine <kate.shaidurova@gmail.com>
2020-05-05 20:44:42 +0300

    ES-3161 Changed params in CheckPeriodContext constructor

--- src/main/kotlin/com/procurement/qualification/application/model/period/check/CheckPeriodContext.kt
+++ src/main/kotlin/com/procurement/qualification/application/model/period/check/CheckPeriodContext.kt
@@ -1,5 +1,6 @@
 package com.procurement.qualification.application.model.period.check
 
-import com.procurement.qualification.domain.enums.ProcurementMethod
+import com.procurement.qualification.domain.model.Cpid
+import com.procurement.qualification.domain.model.Ocid
 
-class CheckPeriodContext(val country: String, val pmd: ProcurementMethod)
+class CheckPeriodContext(val cpid: Cpid, val ocid: Ocid)
