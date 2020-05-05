898a3df5ec2508470f1b3303c31b25f1a7f0463f
katherine <kate.shaidurova@gmail.com>
2020-05-05 12:44:40 +0300

    ES-3130 Added savePeriod method to PeriodService.kt

--- src/main/kotlin/com/procurement/qualification/application/service/period/PeriodService.kt
+++ src/main/kotlin/com/procurement/qualification/application/service/period/PeriodService.kt
@@ -1,12 +1,19 @@
 package com.procurement.qualification.application.service.period
 
+import com.procurement.qualification.application.model.period.save.SavePeriodContext
+import com.procurement.qualification.application.model.period.save.SavePeriodData
+import com.procurement.qualification.application.model.period.save.SavePeriodResult
 import com.procurement.qualification.application.model.period.validate.ValidatePeriodContext
 import com.procurement.qualification.application.model.period.validate.ValidatePeriodData
 import com.procurement.qualification.application.repository.PeriodRepository
 import com.procurement.qualification.application.repository.PeriodRulesRepository
 import com.procurement.qualification.application.service.period.strategy.ValidatePeriodStrategy
+import com.procurement.qualification.domain.functional.Result
+import com.procurement.qualification.domain.functional.Result.Companion.failure
 import com.procurement.qualification.domain.functional.ValidationResult
+import com.procurement.qualification.domain.functional.asSuccess
 import com.procurement.qualification.infrastructure.fail.Fail
+import com.procurement.qualification.infrastructure.model.entity.PeriodEntity
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,4 +24,17 @@ class PeriodService(
 
     fun validatePeriod(data: ValidatePeriodData, context: ValidatePeriodContext): ValidationResult<Fail> =
         validatePeriodStrategy.execute(data = data, context = context)
+
+    fun savePeriod(data: SavePeriodData, context: SavePeriodContext): Result<SavePeriodResult, Fail> {
+        periodRepository.saveOrUpdatePeriod(
+            PeriodEntity(
+                cpid = context.cpid,
+                ocid = context.ocid,
+                endDate = data.period.endDate,
+                startDate = data.period.startDate
+            )
+        ).doOnError { incident -> return failure(incident) }
+
+        return SavePeriodResult.asSuccess()
+    }
 }
