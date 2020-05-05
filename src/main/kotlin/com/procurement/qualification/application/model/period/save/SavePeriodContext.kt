f3bd04c219b151ab2594975a77afab971388f694
katherine <kate.shaidurova@gmail.com>
2020-05-05 12:43:31 +0300

    ES-3130 Added SavePeriod data, context and converter

---
+++ src/main/kotlin/com/procurement/qualification/application/model/period/save/SavePeriodContext.kt
@@ -0,0 +1,7 @@
+package com.procurement.qualification.application.model.period.save
+
+import com.procurement.qualification.domain.enums.Stage
+import com.procurement.qualification.domain.model.Cpid
+import com.procurement.qualification.domain.model.Ocid
+
+class SavePeriodContext(val cpid: Cpid, val ocid: Ocid, val stage: Stage)
