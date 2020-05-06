34021c338c7efccc5667a4dc09b71de56235a58a
katherine <kate.shaidurova@gmail.com>
2020-05-06 13:01:35 +0300

    ES-3161 Fixed validatePeriod tests, added checkPeriod tests

--- src/test/kotlin/com/procurement/qualification/application/service/period/PeriodServiceTest.kt
+++ src/test/kotlin/com/procurement/qualification/application/service/period/PeriodServiceTest.kt
@@ -4,28 +4,32 @@ import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
+import com.procurement.qualification.application.model.period.check.CheckPeriodContext
+import com.procurement.qualification.application.model.period.check.CheckPeriodData
+import com.procurement.qualification.application.model.period.check.CheckPeriodResult
 import com.procurement.qualification.application.model.period.save.SavePeriodContext
 import com.procurement.qualification.application.model.period.save.SavePeriodData
 import com.procurement.qualification.application.model.period.save.SavePeriodResult
 import com.procurement.qualification.application.model.period.validate.ValidatePeriodContext
 import com.procurement.qualification.application.model.period.validate.ValidatePeriodData
 import com.procurement.qualification.application.repository.PeriodRepository
-import com.procurement.qualification.application.repository.PeriodRulesRepository
+import com.procurement.qualification.application.service.period.strategy.CheckPeriodStrategy
+import com.procurement.qualification.application.service.period.strategy.ValidatePeriodStrategy
 import com.procurement.qualification.domain.enums.ProcurementMethod
 import com.procurement.qualification.domain.functional.ValidationResult
 import com.procurement.qualification.domain.functional.asSuccess
 import com.procurement.qualification.domain.model.Cpid
 import com.procurement.qualification.domain.model.Ocid
 import com.procurement.qualification.infrastructure.fail.Fail
-import com.procurement.qualification.infrastructure.fail.error.ValidationError
 import com.procurement.qualification.infrastructure.model.entity.PeriodEntity
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.ResolverStyle
-import java.util.concurrent.TimeUnit
 
 internal class PeriodServiceTest {
 
@@ -35,126 +39,55 @@ internal class PeriodServiceTest {
 
         private val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern(FORMAT_PATTERN)
             .withResolverStyle(ResolverStyle.STRICT)
-        private val DATE = LocalDateTime.parse("2020-02-10T08:49:55Z", FORMATTER)
+        private val REQUEST_START_DATE = LocalDateTime.parse("2020-02-10T08:49:55Z", FORMATTER)
+        private val REQUEST_END_DATE = REQUEST_START_DATE.plusDays(10)
         private val CPID = Cpid.tryCreateOrNull("ocds-t1s2t3-MD-1565251033096")!!
         private val OCID = Ocid.tryCreateOrNull("ocds-b3wdp1-MD-1581509539187-EV-1581509653044")!!
         private val PMD = ProcurementMethod.GPA
-        private val ALLOWED_TERM = TimeUnit.DAYS.toSeconds(10)
     }
 
     private val periodRepository: PeriodRepository = mock()
-    private val periodRulesRepository: PeriodRulesRepository = mock()
-    private val periodService = PeriodService(periodRepository, periodRulesRepository)
+    private val checkPeriodStrategy: CheckPeriodStrategy = mock()
+    private val validatePeriodStrategy: ValidatePeriodStrategy = mock()
+    private val periodService = PeriodService(periodRepository, validatePeriodStrategy, checkPeriodStrategy)
 
     @Nested
     inner class ValidatePeriod {
 
         @Test
-        fun periodDurationEqualsAllowedTerm_success() {
-            whenever(periodRulesRepository.findTermBy(pmd = PMD, country = COUNTRY))
-                .thenReturn(ALLOWED_TERM.asSuccess())
+        @DisplayName("Check that result of validatePeriodStrategy.execute() call is returned")
+        fun success() {
+            val expected = ValidationResult.ok<Fail>()
 
-            val endDate = DATE.plusDays(10).format(FORMATTER)
-            val startDate = DATE.format(FORMATTER)
-            val data = createValidatePeriodData(startDate = startDate, endDate = endDate)
+            val data = stubData()
             val context = stubContext()
 
-            val actual = periodService.validatePeriod(data = data, context = context)
+            whenever(validatePeriodStrategy.execute(data, context)).thenReturn(expected)
 
-            assertTrue(actual.isOk)
+            val actual = periodService.validatePeriod(data, context)
+
+            assertTrue(expected === actual)
         }
 
-        @Test
-        fun periodDurationGreaterThanAllowedTerm_success() {
-            whenever(periodRulesRepository.findTermBy(pmd = PMD, country = COUNTRY))
-                .thenReturn(ALLOWED_TERM.asSuccess())
-
-            val endDate = DATE.plusDays(10).plusSeconds(1).format(FORMATTER)
-            val startDate = DATE.format(FORMATTER)
-            val data = createValidatePeriodData(startDate = startDate, endDate = endDate)
-            val context = stubContext()
-
-            val actual = periodService.validatePeriod(data = data, context = context)
-
-            assertTrue(actual.isOk)
-        }
-
-        @Test
-        fun startAndEndDateEqual_fail() {
-            whenever(periodRulesRepository.findTermBy(pmd = PMD, country = COUNTRY))
-                .thenReturn(ALLOWED_TERM.asSuccess())
-
-            val date = DATE.format(FORMATTER)
-            val data = createValidatePeriodData(startDate = date, endDate = date)
-            val context = stubContext()
-
-            val actual = periodService.validatePeriod(data = data, context = context)
-
-            assertTrue(actual.error is ValidationError.CommandError.InvalidPeriod)
-        }
-
-        @Test
-        fun endDatePrecedesStartDate_fail() {
-            whenever(periodRulesRepository.findTermBy(pmd = PMD, country = COUNTRY))
-                .thenReturn(ALLOWED_TERM.asSuccess())
-
-            val startDate = DATE.plusDays(10).plusSeconds(1).format(FORMATTER)
-            val endDate = DATE.format(FORMATTER)
-
-            val data = createValidatePeriodData(startDate = startDate, endDate = endDate)
-            val context = stubContext()
-
-            val actual = periodService.validatePeriod(data = data, context = context)
-
-            assertTrue(actual.error is ValidationError.CommandError.InvalidPeriod)
-        }
-
-        @Test
-        fun periodDurationLessThanTenDaysByOneSecond_fail() {
-            whenever(periodRulesRepository.findTermBy(pmd = PMD, country = COUNTRY))
-                .thenReturn(ALLOWED_TERM.asSuccess())
-
-            val endDate = DATE.plusDays(10).minusSeconds(1).format(FORMATTER)
-            val startDate = DATE.format(FORMATTER)
-            val data = createValidatePeriodData(startDate = startDate, endDate = endDate)
-            val context = stubContext()
-
-            val actual = periodService.validatePeriod(data = data, context = context)
-
-            assertTrue(actual.error is ValidationError.CommandError.InvalidPeriodTerm)
-        }
-
-        @Test
-        fun periodDurationRuleNotFound_fail() {
-            whenever(periodRulesRepository.findTermBy(pmd = PMD, country = COUNTRY))
-                .thenReturn(null.asSuccess())
-
-            val endDate = DATE.plusDays(10).format(FORMATTER)
-            val startDate = DATE.format(FORMATTER)
-            val data = createValidatePeriodData(startDate = startDate, endDate = endDate)
-            val context = stubContext()
-
-            val actual = periodService.validatePeriod(data = data, context = context)
-
-            assertTrue(actual.error is ValidationError.CommandError.PeriodRuleNotFound)
-        }
-
-        private fun createValidatePeriodData(startDate: String, endDate: String): ValidatePeriodData {
+        private fun stubData(): ValidatePeriodData {
+            val startDate = LocalDateTime.now()
+            val startDateFormatted = startDate.format(FORMATTER)
+            val endDataFormatted = startDate.plusDays(3).format(FORMATTER)
             val period = ValidatePeriodData.Period.tryCreate(
-                startDate = startDate, endDate = endDate
+                startDate = startDateFormatted, endDate = endDataFormatted
             ).get
             return ValidatePeriodData.tryCreate(period).get
         }
 
-        private fun stubContext() = ValidatePeriodContext(pmd = PMD, country = COUNTRY)
+        private fun stubContext() = ValidatePeriodContext(country = COUNTRY, pmd = PMD)
     }
 
     @Nested
     inner class SavePeriod {
         @Test
         fun repositoryMethod_isCalled() {
-            val endDate = DATE.plusDays(10).format(FORMATTER)
-            val startDate = DATE.format(FORMATTER)
+            val startDate = REQUEST_START_DATE.format(FORMATTER)
+            val endDate = REQUEST_END_DATE.format(FORMATTER)
             val data = createSavePeriodData(startDate = startDate, endDate = endDate)
             val context = SavePeriodContext(cpid = CPID, ocid = OCID)
 
@@ -173,8 +106,8 @@ internal class PeriodServiceTest {
 
         @Test
         fun success() {
-            val endDate = DATE.plusDays(10).format(FORMATTER)
-            val startDate = DATE.format(FORMATTER)
+            val startDate = REQUEST_START_DATE.format(FORMATTER)
+            val endDate = REQUEST_END_DATE.format(FORMATTER)
             val data = createSavePeriodData(startDate = startDate, endDate = endDate)
             val context = SavePeriodContext(cpid = CPID, ocid = OCID)
 
@@ -186,8 +119,8 @@ internal class PeriodServiceTest {
 
         @Test
         fun repositoryReturnedError_fail() {
-            val endDate = DATE.plusDays(10).format(FORMATTER)
-            val startDate = DATE.format(FORMATTER)
+            val startDate = REQUEST_START_DATE.format(FORMATTER)
+            val endDate = REQUEST_END_DATE.format(FORMATTER)
             val data = createSavePeriodData(startDate = startDate, endDate = endDate)
             val context = SavePeriodContext(cpid = CPID, ocid = OCID)
 
@@ -205,5 +138,42 @@ internal class PeriodServiceTest {
             ).get
             return SavePeriodData.tryCreate(period).get
         }
+    }
+
+    @Nested
+    inner class CheckPeriod {
+        @Test
+        @DisplayName("Check that result of checkPeriodStrategy.execute() call is returned")
+        fun success() {
+            val expected = CheckPeriodResult(
+                preQualificationPeriodChanged = true,
+                preQualification = CheckPeriodResult.PreQualification(
+                    period = CheckPeriodResult.PreQualification.Period(
+                        startDate = LocalDateTime.now(), endDate = LocalDateTime.now()
+                    )
+                )
+            )
+
+            val data = stubCheckPeriodData()
+            val context = stubContext()
+
+            whenever(checkPeriodStrategy.execute(data, context)).thenReturn(expected.asSuccess())
+
+            val actual = periodService.checkPeriod(data, context).get
+
+            assertTrue(expected === actual)
+            assertEquals(expected, actual)
+        }
+
+        private fun stubCheckPeriodData(): CheckPeriodData {
+            val startDate = REQUEST_START_DATE.format(FORMATTER)
+            val endDate = REQUEST_END_DATE.format(FORMATTER)
+            val period = CheckPeriodData.Period.tryCreate(
+                startDate = startDate, endDate = endDate
+            ).get
+            return CheckPeriodData.tryCreate(period).get
+        }
+
+        private fun stubContext() = CheckPeriodContext(cpid = CPID, ocid = OCID)
     }
 }
