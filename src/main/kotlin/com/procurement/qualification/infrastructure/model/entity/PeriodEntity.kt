e334e49ab54a58826b49d797a9554c16169a5a10
Yevhenii Skoryi <yevheniiskoryi@gmail.com>
2020-05-26 15:39:17 +0300

    ES-3326 marked PeriodEntity.endDate nullable type

--- src/main/kotlin/com/procurement/qualification/infrastructure/model/entity/PeriodEntity.kt
+++ src/main/kotlin/com/procurement/qualification/infrastructure/model/entity/PeriodEntity.kt
@@ -1,5 +1,6 @@
 package com.procurement.qualification.infrastructure.model.entity
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.procurement.qualification.domain.model.Cpid
 import com.procurement.qualification.domain.model.Ocid
 import java.time.LocalDateTime
@@ -8,5 +9,7 @@ data class PeriodEntity(
     val cpid: Cpid,
     val ocid: Ocid,
     val startDate: LocalDateTime,
-    val endDate: LocalDateTime
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val endDate: LocalDateTime? = null
 )
