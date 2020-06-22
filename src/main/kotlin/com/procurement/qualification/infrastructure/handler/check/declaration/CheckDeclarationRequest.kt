21119145d2d3c87930609a9f11b191de469045a2
yevhenii <yevheniiskoryi@gmail.com>
2020-06-22 18:16:56 +0300

    ES-3575 added request dto with mapping tests for command 'checkDeclaration'

---
+++ src/main/kotlin/com/procurement/qualification/infrastructure/handler/check/declaration/CheckDeclarationRequest.kt
@@ -0,0 +1,36 @@
+package com.procurement.qualification.infrastructure.handler.check.declaration
+
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.qualification.domain.model.qualification.RequirementResponseValue
+
+data class CheckDeclarationRequest(
+    @param:JsonProperty("cpid") @field:JsonProperty("cpid") val cpid: String,
+    @param:JsonProperty("ocid") @field:JsonProperty("ocid") val ocid: String,
+    @param:JsonProperty("qualificationId") @field:JsonProperty("qualificationId") val qualificationId: String,
+    @param:JsonProperty("requirementResponse") @field:JsonProperty("requirementResponse") val requirementResponse: RequirementResponse,
+    @param:JsonProperty("criteria") @field:JsonProperty("criteria") val criteria: List<Criteria>
+) {
+    data class RequirementResponse(
+        @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+        @param:JsonProperty("value") @field:JsonProperty("value") val value: RequirementResponseValue,
+        @param:JsonProperty("relatedTendererId") @field:JsonProperty("relatedTendererId") val relatedTendererId: String,
+        @param:JsonProperty("responderId") @field:JsonProperty("responderId") val responderId: String,
+        @param:JsonProperty("requirementId") @field:JsonProperty("requirementId") val requirementId: String
+    )
+
+    data class Criteria(
+        @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+        @param:JsonProperty("requirementGroups") @field:JsonProperty("requirementGroups") val requirementGroups: List<RequirementGroup>
+    ) {
+        data class RequirementGroup(
+            @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+            @param:JsonProperty("requirements") @field:JsonProperty("requirements") val requirements: List<Requirement>
+        ) {
+            data class Requirement(
+                @param:JsonProperty("id") @field:JsonProperty("id") val id: String,
+                @param:JsonProperty("dataType") @field:JsonProperty("dataType") val dataType: String
+            )
+        }
+    }
+}
