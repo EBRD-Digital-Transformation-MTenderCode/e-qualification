ef6334f90294015c08b7deef509e3035b107b35d
yevhenii <yevheniiskoryi@gmail.com>
2020-06-22 14:56:45 +0300

    ES-3577 created application level dto and converter for command 'doDeclaration'

---
+++ src/main/kotlin/com/procurement/qualification/application/model/params/DoDeclarationParams.kt
@@ -0,0 +1,109 @@
+package com.procurement.qualification.application.model.params
+
+import com.procurement.qualification.application.model.parseCpid
+import com.procurement.qualification.application.model.parseOcid
+import com.procurement.qualification.application.model.parseQualificationId
+import com.procurement.qualification.domain.functional.Result
+import com.procurement.qualification.domain.functional.asSuccess
+import com.procurement.qualification.domain.model.Cpid
+import com.procurement.qualification.domain.model.Ocid
+import com.procurement.qualification.domain.model.qualification.QualificationId
+import com.procurement.qualification.domain.model.qualification.RequirementResponseValue
+import com.procurement.qualification.infrastructure.fail.error.DataErrors
+
+class DoDeclarationParams private constructor(
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val qualifications: List<Qualification>
+) {
+
+    companion object {
+        fun tryCreate(
+            cpid: String,
+            ocid: String,
+            qualifications: List<Qualification>
+        ): Result<DoDeclarationParams, DataErrors> {
+
+            val parsedCpid = parseCpid(value = cpid)
+                .orForwardFail { fail -> return fail }
+
+            val parsedOcid = parseOcid(value = ocid)
+                .orForwardFail { fail -> return fail }
+
+            return DoDeclarationParams(cpid = parsedCpid, ocid = parsedOcid, qualifications = qualifications)
+                .asSuccess()
+        }
+    }
+
+    class Qualification private constructor(
+        val id: QualificationId,
+        val requirementResponses: List<RequirementResponse>
+    ) {
+
+        companion object {
+            fun tryCreate(
+                id: String,
+                requirementResponses: List<RequirementResponse>
+            ): Result<Qualification, DataErrors> {
+                val parsedId = parseQualificationId(value = id)
+                    .orForwardFail { fail -> return fail }
+                return Qualification(id = parsedId, requirementResponses = requirementResponses)
+                    .asSuccess()
+            }
+        }
+
+        class RequirementResponse private constructor(
+            val id: String,
+            val value: RequirementResponseValue,
+            val relatedTenderer: RelatedTenderer,
+            val requirement: Requirement,
+            val responder: Responder
+        ) {
+            companion object {
+                fun tryCreate(
+                    id: String,
+                    value: RequirementResponseValue,
+                    relatedTenderer: RelatedTenderer,
+                    requirement: Requirement,
+                    responder: Responder
+                ): Result<RequirementResponse, DataErrors> {
+                    return RequirementResponse(
+                        id = id,
+                        value = value,
+                        relatedTenderer = relatedTenderer,
+                        requirement = requirement,
+                        responder = responder
+                    )
+                        .asSuccess()
+                }
+            }
+
+            class RelatedTenderer private constructor(val id: String) {
+                companion object {
+                    fun tryCreate(id: String): Result<RelatedTenderer, DataErrors> {
+                        return RelatedTenderer(id = id)
+                            .asSuccess()
+                    }
+                }
+            }
+
+            class Requirement private constructor(val id: String) {
+                companion object {
+                    fun tryCreate(id: String): Result<Requirement, DataErrors> {
+                        return Requirement(id = id)
+                            .asSuccess()
+                    }
+                }
+            }
+
+            class Responder private constructor(val id: String, val name: String) {
+                companion object {
+                    fun tryCreate(id: String, name: String): Result<Responder, DataErrors> {
+                        return Responder(id = id, name = name)
+                            .asSuccess()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/procurement/qualification/infrastructure/handler/create/declaration/DoDeclarationConverter.kt
@@ -0,0 +1,50 @@
+package com.procurement.qualification.infrastructure.handler.create.declaration
+
+import com.procurement.qualification.application.model.params.DoDeclarationParams
+import com.procurement.qualification.domain.functional.Result
+import com.procurement.qualification.infrastructure.fail.error.DataErrors
+
+fun DoDeclarationRequest.convert(): Result<DoDeclarationParams, DataErrors> =
+    DoDeclarationParams.tryCreate(
+        cpid = this.cpid,
+        ocid = this.ocid,
+        qualifications = this.qualifications
+            .map {
+                it.convert()
+                    .orForwardFail { fail -> return fail }
+            }
+    )
+
+fun DoDeclarationRequest.Qualification.convert(): Result<DoDeclarationParams.Qualification, DataErrors> =
+    DoDeclarationParams.Qualification.tryCreate(
+        id = this.id,
+        requirementResponses = this.requirementResponses
+            .map {
+                it.convert()
+                    .orForwardFail { fail -> return fail }
+            }
+    )
+
+fun DoDeclarationRequest.Qualification.RequirementResponse.convert(): Result<DoDeclarationParams.Qualification.RequirementResponse, DataErrors> =
+    DoDeclarationParams.Qualification.RequirementResponse.tryCreate(
+        id = this.id,
+        value = this.value,
+        responder = this.responder
+            .convert()
+            .orForwardFail { fail -> return fail },
+        requirement = this.requirement
+            .convert()
+            .orForwardFail { fail -> return fail },
+        relatedTenderer = this.relatedTenderer
+            .convert()
+            .orForwardFail { fail -> return fail }
+    )
+
+fun DoDeclarationRequest.Qualification.RequirementResponse.RelatedTenderer.convert(): Result<DoDeclarationParams.Qualification.RequirementResponse.RelatedTenderer, DataErrors> =
+    DoDeclarationParams.Qualification.RequirementResponse.RelatedTenderer.tryCreate(id = this.id)
+
+fun DoDeclarationRequest.Qualification.RequirementResponse.Requirement.convert(): Result<DoDeclarationParams.Qualification.RequirementResponse.Requirement, DataErrors> =
+    DoDeclarationParams.Qualification.RequirementResponse.Requirement.tryCreate(id = this.id)
+
+fun DoDeclarationRequest.Qualification.RequirementResponse.Responder.convert(): Result<DoDeclarationParams.Qualification.RequirementResponse.Responder, DataErrors> =
+    DoDeclarationParams.Qualification.RequirementResponse.Responder.tryCreate(id = this.id, name = this.name)
