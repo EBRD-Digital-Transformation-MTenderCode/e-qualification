4219ea76ff5fd35c968f57a392286bed0744e112
yevhenii <yevheniiskoryi@gmail.com>
2020-06-25 13:59:31 +0300

    ES-3592 marked 'statusDetails' in States optional

--- src/main/kotlin/com/procurement/qualification/domain/model/state/States.kt
+++ src/main/kotlin/com/procurement/qualification/domain/model/state/States.kt
@@ -1,5 +1,6 @@
 package com.procurement.qualification.domain.model.state
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.procurement.qualification.domain.enums.QualificationStatus
 import com.procurement.qualification.domain.enums.QualificationStatusDetails
@@ -7,6 +8,7 @@ import com.procurement.qualification.domain.enums.QualificationStatusDetails
 class States(states: List<State>) : List<States.State> by states {
     data class State(
         @param:JsonProperty("status") @field:JsonProperty("status") val status: QualificationStatus,
-        @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: QualificationStatusDetails
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        @param:JsonProperty("statusDetails") @field:JsonProperty("statusDetails") val statusDetails: QualificationStatusDetails?
     )
 }
