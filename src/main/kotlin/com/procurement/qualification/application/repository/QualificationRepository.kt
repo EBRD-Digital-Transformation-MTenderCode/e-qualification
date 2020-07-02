0312b0b0129bd9e99645675fa20bf89041cb2fde
katherine <kate.shaidurova@gmail.com>
2020-07-02 12:11:51 +0300

    ES-3653 Replaced QualificationEntity in QualificationRepository methods' signature with Qualification

--- src/main/kotlin/com/procurement/qualification/application/repository/QualificationRepository.kt
+++ src/main/kotlin/com/procurement/qualification/application/repository/QualificationRepository.kt
@@ -4,19 +4,23 @@ import com.procurement.qualification.domain.functional.MaybeFail
 import com.procurement.qualification.domain.functional.Result
 import com.procurement.qualification.domain.model.Cpid
 import com.procurement.qualification.domain.model.Ocid
+import com.procurement.qualification.domain.model.qualification.Qualification
 import com.procurement.qualification.domain.model.qualification.QualificationId
 import com.procurement.qualification.infrastructure.fail.Fail
-import com.procurement.qualification.infrastructure.model.entity.QualificationEntity
 
 interface QualificationRepository {
 
-    fun findBy(cpid: Cpid, ocid: Ocid): Result<List<QualificationEntity>, Fail.Incident>
+    fun findBy(cpid: Cpid, ocid: Ocid): Result<List<Qualification>, Fail.Incident>
 
-    fun findBy(cpid: Cpid, ocid: Ocid,qualificationId: QualificationId): Result<QualificationEntity?, Fail.Incident>
+    fun findBy(cpid: Cpid, ocid: Ocid, qualificationId: QualificationId): Result<Qualification?, Fail.Incident>
 
-    fun save(entity: QualificationEntity): MaybeFail<Fail.Incident>
+    fun findBy(
+        cpid: Cpid, ocid: Ocid, qualificationIds: List<QualificationId>
+    ): Result<List<Qualification>, Fail.Incident>
 
-    fun saveAll(entities: List<QualificationEntity>): MaybeFail<Fail.Incident>
+    fun save(cpid: Cpid, ocid: Ocid, qualification: Qualification): MaybeFail<Fail.Incident>
 
-    fun updateAll(entities: List<QualificationEntity>): MaybeFail<Fail.Incident>
+    fun saveAll(cpid: Cpid, ocid: Ocid, qualifications: List<Qualification>): MaybeFail<Fail.Incident>
+
+    fun updateAll(cpid: Cpid, ocid: Ocid, qualifications: List<Qualification>): MaybeFail<Fail.Incident>
 }
