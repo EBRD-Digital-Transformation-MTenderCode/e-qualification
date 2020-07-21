3980084dec677054a1dccafe19458babd22e1f99
Evgen Kopach <zkopach@gmail.com>
2020-07-21 14:10:52 +0300

    ES-3815 Create Params DTO for 'setQualificationPeriodEnd' command

---
+++ src/main/kotlin/com/procurement/qualification/application/model/params/SetQualificationPeriodEndParams.kt
@@ -0,0 +1,43 @@
+package com.procurement.qualification.application.model.params
+
+import com.procurement.qualification.application.model.parseCpid
+import com.procurement.qualification.application.model.parseOcid
+import com.procurement.qualification.domain.functional.Result
+import com.procurement.qualification.domain.functional.asSuccess
+import com.procurement.qualification.domain.model.Cpid
+import com.procurement.qualification.domain.model.Ocid
+import com.procurement.qualification.domain.util.extension.tryParseLocalDateTime
+import com.procurement.qualification.infrastructure.fail.error.DataErrors
+import java.time.LocalDateTime
+
+class SetQualificationPeriodEndParams private constructor(
+    val cpid: Cpid,
+    val ocid: Ocid,
+    val date: LocalDateTime
+) {
+
+    companion object {
+        fun tryCreate(cpid: String, ocid: String, date: String): Result<SetQualificationPeriodEndParams, DataErrors> {
+
+            val parsedCpid = parseCpid(value = cpid)
+                .orForwardFail { fail -> return fail }
+
+            val parsedOcid = parseOcid(value = ocid)
+                .orForwardFail { fail -> return fail }
+
+            val parsedDate = date.tryParseLocalDateTime()
+                .doReturn { pattern ->
+                    return Result.failure(
+                        DataErrors.Validation.DataFormatMismatch(
+                            name = "date",
+                            actualValue = date,
+                            expectedFormat = pattern
+                        )
+                    )
+                }
+
+            return SetQualificationPeriodEndParams(cpid = parsedCpid, ocid = parsedOcid, date = parsedDate)
+                .asSuccess()
+        }
+    }
+}
