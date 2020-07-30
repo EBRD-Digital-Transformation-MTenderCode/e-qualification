e8406a6bad1bd4ef871e798aeae35a7911ecb6aa
Evgen Kopach <zkopach@gmail.com>
2020-07-30 10:03:06 +0300

    ES-3815 Removed duplicated enum elements in Command2Type.kt

--- src/main/kotlin/com/procurement/qualification/infrastructure/web/enums/Command2Type.kt
+++ src/main/kotlin/com/procurement/qualification/infrastructure/web/enums/Command2Type.kt
@@ -22,7 +22,6 @@ enum class Command2Type(@JsonValue override val key: String) : Action, EnumEleme
     FIND_REQUIREMENT_RESPONSE_BY_IDS("findRequirementResponseByIds"),
     RANK_QUALIFICATIONS("rankQualifications"),
     SET_NEXT_FOR_QUALIFICATION("setNextForQualification"),
-    START_QUALIFICATION_PERIOD("startQualificationPeriod"),
     SET_QUALIFICATION_PERIOD_END("setQualificationPeriodEnd"),
     START_QUALIFICATION_PERIOD("startQualificationPeriod");
 
